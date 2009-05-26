80db68f3815ff44fe91ec060a7e3b1dab96c1369
TASCA collaboration <tasca@gsi.de>
2009-05-26 09:52:20 +0000

    Prefix in calibration
    git-svn-id: https://subversion.gsi.de/go4/trunk@140 302e7798-3865-0410-85c9-d38765bbeff6

--- applications/tasca/setcali.C
+++ applications/tasca/setcali.C
@@ -5,5 +5,8 @@ void setcali()
 {
 Bool_t on = kFALSE; // set value here
 TascaCalibration *fCalibration = (TascaCalibration *) go4->GetObject("Calibration","Go4");
-if(fCalibration!=0)  fCalibration->enableCalibration(on);
+ if(fCalibration!=0){
+  fCalibration->EnableCalibration(on);
+  fCalibration->SetPrefix("new");// prefix for coefficient files
+ }
 }
