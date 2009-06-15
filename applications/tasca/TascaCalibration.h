6e31123f7663abbb63a48cc3194f78962a34efab
TASCA collaboration <tasca@gsi.de>
2009-06-15 17:14:38 +0000

    Hit indices default -1, multiplicity
    git-svn-id: https://subversion.gsi.de/go4/trunk@236 302e7798-3865-0410-85c9-d38765bbeff6

--- applications/tasca/TascaCalibration.h
+++ applications/tasca/TascaCalibration.h
@@ -30,36 +30,44 @@ class TascaCalibration : public TGo4Parameter {
     	  Double_t c=(Double_t)channel;
     	  return (UInt_t) (fdGammaT_a0+fdGammaT_a1*c);
       }
-      inline Float_t CalibrateStopXL(UInt_t channel,UInt_t index){
+      inline Float_t CalibrateStopXL(UInt_t channel,Int_t index){
     	  Double_t c=(Double_t)channel;
+	  if(index < 0) index=0;
     	  return (Float_t) (fdStopXL_a0[index]+(fdStopXL_a1[index]+fdStopXL_a2[index]*c)*c);
       }
-      inline Float_t CalibrateStopXH(UInt_t channel,UInt_t index){
+      inline Float_t CalibrateStopXH(UInt_t channel,Int_t index){
     	  Double_t c=(Double_t)channel;
+	  if(index < 0) index=0;
     	  return (Float_t) (fdStopXH_a0[index]+(fdStopXH_a1[index]+fdStopXH_a2[index]*c)*c);
       }
-      inline Float_t CalibrateStopYL(UInt_t channel,UInt_t index){
+      inline Float_t CalibrateStopYL(UInt_t channel,Int_t index){
     	  Double_t c=(Double_t)channel;
+	  if(index < 0) index=0;
     	  return (Float_t) (fdStopYL_a0[index]+(fdStopYL_a1[index]+fdStopYL_a2[index]*c)*c);
       }
-      inline Float_t CalibrateStopYH(UInt_t channel,UInt_t index){
+      inline Float_t CalibrateStopYH(UInt_t channel,Int_t index){
     	  Double_t c=(Double_t)channel;
+	  if(index < 0) index=0;
     	  return (Float_t) (fdStopYH_a0[index]+(fdStopYH_a1[index]+fdStopYH_a2[index]*c)*c);
       }
-      inline Float_t CalibrateBackL(UInt_t channel,UInt_t index){
+      inline Float_t CalibrateBackL(UInt_t channel,Int_t index){
     	  Double_t c=(Double_t)channel;
+	  if(index < 0) index=0;
     	  return (Float_t) (fdBackL_a0[index]+(fdBackL_a1[index]+fdBackL_a2[index]*c)*c);
       }
-      inline Float_t CalibrateBackH(UInt_t channel,UInt_t index){
+      inline Float_t CalibrateBackH(UInt_t channel,Int_t index){
     	  Double_t c=(Double_t)channel;
+	  if(index < 0) index=0;
     	  return (Float_t) (fdBackH_a0[index]+(fdBackH_a1[index]+fdBackH_a2[index]*c)*c);
       }
-      inline Float_t CalibrateVetoL(UInt_t channel,UInt_t index){
+      inline Float_t CalibrateVetoL(UInt_t channel,Int_t index){
     	  Double_t c=(Double_t)channel;
+	  if(index < 0) index=0;
     	  return (Float_t) (fdVetoL_a0[index]+(fdVetoL_a1[index]+fdVetoL_a2[index]*c)*c);
       }
-      inline Float_t CalibrateVetoH(UInt_t channel,UInt_t index){
+      inline Float_t CalibrateVetoH(UInt_t channel,Int_t index){
     	  Double_t c=(Double_t)channel;
+	  if(index < 0) index=0;
     	  return (Float_t) (fdVetoH_a0[index]+(fdVetoH_a1[index]+fdVetoH_a2[index]*c)*c);
       }
       void EnableCalibration(Bool_t on){
