dea88fc359e0cf59115ad528f4b1113ad6fb9174
Sergey Linev <S.Linev@gsi.de>
2009-11-04 12:30:14 +0000

    1. Set svn:keywords property for new files 2. Remove double copyright header
    git-svn-id: https://subversion.gsi.de/go4/trunk@490 302e7798-3865-0410-85c9-d38765bbeff6

--- qt4/Go4FitGUI/QFitModelPolynomWidget.h
+++ qt4/Go4FitGUI/QFitModelPolynomWidget.h
@@ -11,19 +11,6 @@
 // in Go4License.txt file which is part of the distribution.
 //-----------------------------------------------------------------------
 
-// $Id$
-//-----------------------------------------------------------------------
-//       The GSI Online Offline Object Oriented (Go4) Project
-//         Experiment Data Processing at EE department, GSI
-//-----------------------------------------------------------------------
-// Copyright (C) 2000- GSI Helmholtzzentrum für Schwerionenforschung GmbH
-//                     Planckstr. 1, 64291 Darmstadt, Germany
-// Contact:            http://go4.gsi.de
-//-----------------------------------------------------------------------
-// This software can be used under the license agreements as stated
-// in Go4License.txt file which is part of the distribution.
-//-----------------------------------------------------------------------
-
 #ifndef QFITMODELPOLYNOMWIDGET_H
 #define QFITMODELPOLYNOMWIDGET_H
 
@@ -35,8 +22,6 @@
 #include <QFitModelWidget.h>
 
 class TGo4FitModelPolynom;
-
-
 
 class QFitModelPolynomWidget : public QFitModelWidget, public Ui::QFitModelPolynomWidget
  {
--- qt4/Go4FitGUI/QFitNamedWidget.cpp
+++ qt4/Go4FitGUI/QFitNamedWidget.cpp
@@ -11,22 +11,9 @@
 // in Go4License.txt file which is part of the distribution.
 //-----------------------------------------------------------------------
 
-// $Id$
-//-----------------------------------------------------------------------
-//       The GSI Online Offline Object Oriented (Go4) Project
-//         Experiment Data Processing at EE department, GSI
-//-----------------------------------------------------------------------
-// Copyright (C) 2000- GSI Helmholtzzentrum für Schwerionenforschung GmbH
-//                     Planckstr. 1, 64291 Darmstadt, Germany
-// Contact:            http://go4.gsi.de
-//-----------------------------------------------------------------------
-// This software can be used under the license agreements as stated
-// in Go4License.txt file which is part of the distribution.
-//-----------------------------------------------------------------------
-
 #include "QFitNamedWidget.h"
 
-#include "qlabel.h"
+#include <QLabel>
 
 #include "QGo4LineEdit.h"
 #include "QFitItem.h"
