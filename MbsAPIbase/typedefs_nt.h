169cadffedaa546b154c1c9a76a136364b782065
Sergey Linev <S.Linev@gsi.de>
2011-01-18 20:12:50 +0000

    win32: fix for older VC2008
    git-svn-id: https://subversion.gsi.de/go4/trunk@693 302e7798-3865-0410-85c9-d38765bbeff6

--- MbsAPIbase/typedefs_nt.h
+++ MbsAPIbase/typedefs_nt.h
@@ -13,56 +13,157 @@
 
 /* This file called in typedefs.h defines data types for NT */
 
-#ifdef WIN32
 
-
+#ifndef ESOCKTNOSUPPORT
 #define ESOCKTNOSUPPORT         WSAESOCKTNOSUPPORT
-
-
-
-/* #define EWOULDBLOCK             WSAEWOULDBLOCK
-#define EINPROGRESS             WSAEINPROGRESS
-#define EALREADY                WSAEALREADY
-#define ENOTSOCK                WSAENOTSOCK
-#define EDESTADDRREQ            WSAEDESTADDRREQ
-#define EMSGSIZE                WSAEMSGSIZE
-#define EPROTOTYPE              WSAEPROTOTYPE
-#define ENOPROTOOPT             WSAENOPROTOOPT
-#define EPROTONOSUPPORT         WSAEPROTONOSUPPORT
-#define ESOCKTNOSUPPORT         WSAESOCKTNOSUPPORT
-#define EOPNOTSUPP              WSAEOPNOTSUPP
-#define EPFNOSUPPORT            WSAEPFNOSUPPORT
-#define EAFNOSUPPORT            WSAEAFNOSUPPORT
-#define EADDRINUSE              WSAEADDRINUSE
-#define EADDRNOTAVAIL           WSAEADDRNOTAVAIL
-#define ENETDOWN                WSAENETDOWN
-#define ENETUNREACH             WSAENETUNREACH
-#define ENETRESET               WSAENETRESET
-#define ECONNABORTED            WSAECONNABORTED
-#define ECONNRESET              WSAECONNRESET
-#define ENOBUFS                 WSAENOBUFS
-#define EISCONN                 WSAEISCONN
-#define ENOTCONN                WSAENOTCONN
-#define ESHUTDOWN               WSAESHUTDOWN
-#define ETOOMANYREFS            WSAETOOMANYREFS
-#define ETIMEDOUT               WSAETIMEDOUT
-#define ECONNREFUSED            WSAECONNREFUSED
-#define ELOOP                   WSAELOOP
-*/
-
-/*#define ENAMETOOLONG            WSAENAMETOOLONG*/
-
-/*
-#define EHOSTDOWN               WSAEHOSTDOWN
-#define EHOSTUNREACH            WSAEHOSTUNREACH
-*/
-/*#define ENOTEMPTY               WSAENOTEMPTY*/
-/*
-#define EPROCLIM                WSAEPROCLIM
-#define EUSERS                  WSAEUSERS
-#define EDQUOT                  WSAEDQUOT
-#define ESTALE                  WSAESTALE
-#define EREMOTE                 WSAEREMOTE
-*/
 #endif
+
+#ifndef ECONNRESET
+#define ECONNRESET              WSAECONNRESET
+#endif
+
+#ifndef ENOTSOCK
+#define ENOTSOCK                WSAENOTSOCK
+#endif
+
+#ifndef EAFNOSUPPORT
+#define EAFNOSUPPORT            WSAEAFNOSUPPORT
+#endif
+
+#ifndef EPROTONOSUPPORT
+#define EPROTONOSUPPORT         WSAEPROTONOSUPPORT
+#endif
+
+#ifndef ENOBUFS
+#define ENOBUFS                 WSAENOBUFS
+#endif
+
+#ifndef EISCONN
+#define EISCONN                 WSAEISCONN
+#endif
+
+#ifndef ETIMEDOUT
+#define ETIMEDOUT               WSAETIMEDOUT
+#endif
+
+#ifndef ENETUNREACH
+#define ENETUNREACH             WSAENETUNREACH
+#endif
+
+#ifndef EADDRINUSE
+#define EADDRINUSE              WSAEADDRINUSE
+#endif
+
+#ifndef ECONNREFUSED
+#define ECONNREFUSED            WSAECONNREFUSED
+#endif
+
+
+// these constants not used, but keep them
+
+ 
+#ifndef EWOULDBLOCK
+#define EWOULDBLOCK             WSAEWOULDBLOCK
+#endif
+
+#ifndef EINPROGRESS
+#define EINPROGRESS             WSAEINPROGRESS
+#endif
+
+#ifndef EALREADY
+#define EALREADY                WSAEALREADY
+#endif
+
+#ifndef EDESTADDRREQ
+#define EDESTADDRREQ            WSAEDESTADDRREQ
+#endif
+
+#ifndef EMSGSIZE
+#define EMSGSIZE                WSAEMSGSIZE
+#endif
+
+#ifndef EPROTOTYPE
+#define EPROTOTYPE              WSAEPROTOTYPE
+#endif
+
+#ifndef ENOPROTOOPT
+#define ENOPROTOOPT             WSAENOPROTOOPT
+#endif
+
+#ifndef EOPNOTSUPP
+#define EOPNOTSUPP              WSAEOPNOTSUPP
+#endif
+
+#ifndef EPFNOSUPPORT
+#define EPFNOSUPPORT            WSAEPFNOSUPPORT
+#endif
+
+#ifndef EADDRNOTAVAIL
+#define EADDRNOTAVAIL           WSAEADDRNOTAVAIL
+#endif
+
+#ifndef ENETDOWN
+#define ENETDOWN                WSAENETDOWN
+#endif
+
+#ifndef ENETRESET
+#define ENETRESET               WSAENETRESET
+#endif
+
+#ifndef ECONNABORTED
+#define ECONNABORTED            WSAECONNABORTED
+#endif
+
+#ifndef ENOTCONN
+#define ENOTCONN                WSAENOTCONN
+#endif
+
+#ifndef ESHUTDOWN
+#define ESHUTDOWN               WSAESHUTDOWN
+#endif
+
+#ifndef ETOOMANYREFS
+#define ETOOMANYREFS            WSAETOOMANYREFS
+#endif
+
+#ifndef ELOOP
+#define ELOOP                   WSAELOOP
+#endif
+
+#ifndef ENAMETOOLONG
+#define ENAMETOOLONG            WSAENAMETOOLONG
+#endif
+
+#ifndef EHOSTDOWN
+#define EHOSTDOWN               WSAEHOSTDOWN
+#endif
+
+#ifndef EHOSTUNREACH
+#define EHOSTUNREACH            WSAEHOSTUNREACH
+#endif
+
+#ifndef ENOTEMPTY
+#define ENOTEMPTY               WSAENOTEMPTY
+#endif
+
+#ifndef EPROCLIM
+#define EPROCLIM                WSAEPROCLIM
+#endif
+
+#ifndef EUSERS
+#define EUSERS                  WSAEUSERS
+#endif
+
+#ifndef EDQUOT
+#define EDQUOT                  WSAEDQUOT
+#endif
+
+#ifndef ESTALE
+#define ESTALE                  WSAESTALE
+#endif
+
+#ifndef EREMOTE
+#define EREMOTE                 WSAEREMOTE
+#endif
+
 
