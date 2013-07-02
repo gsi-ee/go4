9dc8a7e0f7f00f3b110a7b3cae9e5bf4e9e4b665
Sergey Linev <S.Linev@gsi.de>
2013-07-02 07:51:40 +0000

    small change in Go4Sniffer
    git-svn-id: https://subversion.gsi.de/go4/trunk@988 302e7798-3865-0410-85c9-d38765bbeff6

--- Go4Dabc/TGo4Sniffer.cxx
+++ Go4Dabc/TGo4Sniffer.cxx
@@ -25,7 +25,7 @@ void* TGo4Sniffer::ScanRootHierarchy(dabc::Hierarchy& h, const char* searchpath)
    void *res = 0;
    if (h.null()) return res;
 
-   if (searchpath==0) h.Field(dabc::prop_kind).SetStr("Go4.Analysis");
+   if (searchpath==0) h.Field(dabc::prop_kind).SetStr("GO4.Analysis");
 
    if (!res) res = ScanListHierarchy(h, searchpath, om->GetObjectFolder()->GetListOfFolders(), 0);
 
