ba3fca9e36ace981a06fc45821abd8320f6c6e3c
Sergey Linev <S.Linev@gsi.de>
2014-11-07 17:06:10 +0000

    Add help info to go4 gui Switch to version number 4.7.1
    git-svn-id: https://subversion.gsi.de/go4/trunk@1272 302e7798-3865-0410-85c9-d38765bbeff6

--- Go4Analysis/TGo4Version.h
+++ Go4Analysis/TGo4Version.h
@@ -21,8 +21,8 @@
 // to check the user analysis against framework version on runtime
 // 10205 means Version 1.02.05 or release number  1.2-5,
 
-#define __GO4BUILDVERSION__ 40700
-#define __GO4RELEASE__ "v4.7.0"
+#define __GO4BUILDVERSION__ 40701
+#define __GO4RELEASE__ "v4.7.1"
 
 class TGo4Version {
    protected:
