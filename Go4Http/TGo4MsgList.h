16972fa097993cc7b3e50d2d4d972ad834a92b7b
Sergey Linev <S.Linev@gsi.de>
2015-06-12 13:29:42 +0000

    Add icon
    git-svn-id: https://subversion.gsi.de/go4/trunk@1541 302e7798-3865-0410-85c9-d38765bbeff6

--- Go4Http/TGo4MsgList.h
+++ Go4Http/TGo4MsgList.h
@@ -43,7 +43,8 @@ class TGo4MsgList : public TNamed {
 
       TList* Select(Long64_t id = 0);
 
-   ClassDef(TGo4MsgList, 1); // Custom message list for web server *SNIFF* _autoload="go4sys/html/go4.js" _make_request="GO4.MakeMsgListRequest" _after_request=GO4.AfterMsgListRequest
+   ClassDef(TGo4MsgList, 1); // Custom message list for web server *SNIFF* _autoload="go4sys/html/go4.js" _make_request="GO4.MakeMsgListRequest" _after_request=GO4.AfterMsgListRequest _icon=img_text
+
 };
 
 
