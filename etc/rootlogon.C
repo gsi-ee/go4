914726ad9de5420047b9f49016024c95dd309bbd
Sergey Linev <S.Linev@gsi.de>
2018-04-19 16:11:29 +0000

    Provide rootlogon.C example
    git-svn-id: https://subversion.gsi.de/go4/trunk@2060 302e7798-3865-0410-85c9-d38765bbeff6

---
+++ etc/rootlogon.C
@@ -0,0 +1,6 @@
+// example of logon script, required to correctly use trees from ROOT macros
+
+{
+   gInterpreter->AddIncludePath("/home/user/go4/");
+   gInterpreter->AddIncludePath("/home/user/go4/include/");
+}
