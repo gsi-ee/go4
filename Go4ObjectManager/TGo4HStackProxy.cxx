e514977601f7bbffaaeea27a83218b7494ee896a
Sergey Linev <S.Linev@gsi.de>
2018-08-14 11:08:22 +0000

    Remove unused include
    git-svn-id: https://subversion.gsi.de/go4/trunk@2131 302e7798-3865-0410-85c9-d38765bbeff6

--- Go4ObjectManager/TGo4HStackProxy.cxx
+++ Go4ObjectManager/TGo4HStackProxy.cxx
@@ -3,7 +3,7 @@
 //       The GSI Online Offline Object Oriented (Go4) Project
 //         Experiment Data Processing at EE department, GSI
 //-----------------------------------------------------------------------
-// Copyright (C) 2000- GSI Helmholtzzentrum für Schwerionenforschung GmbH
+// Copyright (C) 2000- GSI Helmholtzzentrum fï¿½r Schwerionenforschung GmbH
 //                     Planckstr. 1, 64291 Darmstadt, Germany
 // Contact:            http://go4.gsi.de
 //-----------------------------------------------------------------------
@@ -18,7 +18,6 @@
 #include "TClass.h"
 #include "TFrame.h"
 #include "TPaveText.h"
-#include "TObjString.h"
 #include "THStack.h"
 #include "TH1.h"
 
