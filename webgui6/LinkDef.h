81f7109fe49a593523b06b53789040c6cd36b0cd
Sergey Linev <S.Linev@gsi.de>
2019-04-18 15:09:40 +0000

    Add menu items for webcanvas
    git-svn-id: https://subversion.gsi.de/go4/trunk@2394 302e7798-3865-0410-85c9-d38765bbeff6

--- webgui6/LinkDef.h
+++ webgui6/LinkDef.h
@@ -4,6 +4,12 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
+#pragma link C++ class TWebMenuItem+;
+#pragma link C++ class TWebCheckedMenuItem+;
+#pragma link C++ class TWebMenuArgument+;
+#pragma link C++ class TWebArgsMenuItem+;
+#pragma link C++ class TWebMenuItems+;
+
 #pragma link C++ class TWebCanvasFull+;
 #pragma link C++ class TWebObjectOptions1+;
 #pragma link C++ class TWebPadRange1+;
--- webgui6/TWebCanvasFull.cxx
+++ webgui6/TWebCanvasFull.cxx
@@ -1,5 +1,8 @@
 #include "TWebCanvasFull.h"
 
+#include "TWebMenuItem.h"
+#include "TCanvas.h"
+
 TWebCanvasFull::TWebCanvasFull(TCanvas *c, const char *name, Int_t x, Int_t y, UInt_t width, UInt_t height) :
    TWebCanvas(c,name,x,y,width,height)
 {
@@ -11,6 +14,23 @@ Bool_t TWebCanvasFull::ProcessData(unsigned connid, const std::string &arg)
 {
    if (TWebCanvas::ProcessData(connid, arg))
       return kTRUE;
+
+   if (arg.compare(0, 8, "GETMENU:") == 0) {
+
+      TObject *obj = FindPrimitive(arg.c_str() + 8);
+      if (!obj)
+         obj = Canvas();
+
+      TWebMenuItems items;
+      items.PopulateObjectMenu(obj, obj->IsA());
+      std::string buf = "MENU:";
+      buf.append(arg.c_str() + 8);
+      buf.append(":");
+      buf.append(items.ProduceJSON().Data());
+
+      AddToSendQueue(connid, buf);
+   }
+
 
    return kTRUE;
 }
