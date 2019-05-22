5f70a46fa0eee049c5de505c60c8b396e7d0105b
Sergey Linev <S.Linev@gsi.de>
2019-05-22 14:00:44 +0000

    clang warning - virtual method hiding in TGo4ListCond
    git-svn-id: https://subversion.gsi.de/go4/trunk@2462 302e7798-3865-0410-85c9-d38765bbeff6

--- Go4ConditionsBase/TGo4ListCond.h
+++ Go4ConditionsBase/TGo4ListCond.h
@@ -3,7 +3,7 @@
 //       The GSI Online Offline Object Oriented (Go4) Project
 //         Experiment Data Processing at EE department, GSI
 //-----------------------------------------------------------------------
-// Copyright (C) 2000- GSI Helmholtzzentrum für Schwerionenforschung GmbH
+// Copyright (C) 2000- GSI Helmholtzzentrum fï¿½r Schwerionenforschung GmbH
 //                     Planckstr. 1, 64291 Darmstadt, Germany
 // Contact:            http://go4.gsi.de
 //-----------------------------------------------------------------------
@@ -30,18 +30,20 @@ public:
   TGo4ListCond(const char * name, const char * title, const Int_t start, const Int_t stop, const Int_t step = 1);
   virtual ~TGo4ListCond();
 
+  using TGo4Condition::SetValues;
   virtual void SetValues(const Int_t num, const Int_t * values);
   virtual void SetValues(const Int_t start, const Int_t stop, const Int_t step);
 
+  using TGo4Condition::Test;
   virtual Bool_t Test(Int_t value);
 
   void PrintCondition(Bool_t full = kFALSE);
   void PrintValues();
 
-  Bool_t UpdateFrom(TGo4Condition * cond, Bool_t counts);
+  Bool_t UpdateFrom(TGo4Condition *cond, Bool_t counts);
 
-  UInt_t GetNumValues(){return fxValues.size();}
-  Int_t GetValue(UInt_t ix){return ix<fxValues.size() ? fxValues[ix] : 0;}
+  UInt_t GetNumValues() const { return fxValues.size(); }
+  Int_t GetValue(UInt_t ix) const { return ix < fxValues.size() ? fxValues[ix] : 0; }
 
   void SetValue(UInt_t ix, Int_t val)
   {
@@ -82,8 +84,7 @@ private:
 //		/** array with values to test: todo: use vector here?*/
 //		Int_t * fiValues;
 
-ClassDef(TGo4ListCond, 1)
-  ;
+   ClassDef(TGo4ListCond, 1);
 };
 
 #endif // TGO4LISTCOND_H
