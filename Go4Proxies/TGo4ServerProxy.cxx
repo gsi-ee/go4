649e5438828f80fe3ab0567eae23d6fc6df4c503
Sergey Linev <S.Linev@gsi.de>
2019-05-27 12:42:14 +0000

    Add mac support in analysis launching
    git-svn-id: https://subversion.gsi.de/go4/trunk@2506 302e7798-3865-0410-85c9-d38765bbeff6

--- Go4Proxies/TGo4ServerProxy.cxx
+++ Go4Proxies/TGo4ServerProxy.cxx
@@ -3,7 +3,7 @@
 //       The GSI Online Offline Object Oriented (Go4) Project
 //         Experiment Data Processing at EE department, GSI
 //-----------------------------------------------------------------------
-// Copyright (C) 2000- GSI Helmholtzzentrum für Schwerionenforschung GmbH
+// Copyright (C) 2000- GSI Helmholtzzentrum fï¿½r Schwerionenforschung GmbH
 //                     Planckstr. 1, 64291 Darmstadt, Germany
 // Contact:            http://go4.gsi.de
 //-----------------------------------------------------------------------
@@ -34,8 +34,10 @@ class TGo4Prefs {
       TGo4Prefs(const char* hostname)
       {
          SetPar("hostname", hostname);
-         #ifdef WIN32
+         #if defined(WIN32)
          SetPar("os", "win32");
+         #elif defined(Darwin)
+         SetPar("os", "mac");
          #else
          SetPar("os", "linux");
          #endif
