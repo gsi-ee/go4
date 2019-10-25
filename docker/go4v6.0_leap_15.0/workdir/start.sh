d77ab83107acac0411b045fc29ee4216d5c31d1d
Joern Adamczewski <j.adamczewski@gsi.de>
2019-10-25 07:26:50 +0000

    added docker files for go4V6 with open suse leap 15
    git-svn-id: https://subversion.gsi.de/go4/trunk@2652 302e7798-3865-0410-85c9-d38765bbeff6

---
+++ docker/go4v6.0_leap_15.0/workdir/start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "container started"
+
+#echo "create main tmux session"
+
+echo "run /conf/conf.sh"
+. /conf/conf.sh # keep environment variables set in conf.sh
+tail -n 50 /conf/conf_log.txt
+cd /workdir
+
+# open some applications on vnc server:
+sleep 5
+lxpanel & 
+xterm &
+
+echo "drop user to shell"
+/bin/bash
+echo "terminate container"
