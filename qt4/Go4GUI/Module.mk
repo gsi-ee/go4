846da0ece07eca16d7487578a8265d3fcd4724d5
Sergey Linev <S.Linev@gsi.de>
2020-01-09 15:10:15 +0000

    typo
    git-svn-id: https://subversion.gsi.de/go4/trunk@2672 302e7798-3865-0410-85c9-d38765bbeff6

--- qt4/Go4GUI/Module.mk
+++ qt4/Go4GUI/Module.mk
@@ -47,9 +47,9 @@ GO4GUI4_QMAKEFLAGS += "HEADERS += ../Go4Web/QWebCanvas.h"
 GO4GUI4_QMAKEFLAGS += "SOURCES += ../Go4Web/QWebCanvas.cpp" 
 GO4GUI4_QMAKEFLAGS += "QT += webengine webenginewidgets" 
 ifeq ($(GO4_OS),Win32)
-GO4GUI4_QMAKEFLAGS += "LIBS += $(shell cygpath -w $(ROOTSYS)/lib/libRHTTP.lib) \
-                               $(shell cygpath -w $(ROOTSYS)/lib/lROOTWebDisplay.lib) \
-                               $(shell cygpath -w $(ROOTSYS)/lib/lROOTQt5WebDisplaylib) \
+GO4GUI4_QMAKEFLAGS += "LIBS += $(shell cygpath -w $(ROOTSYS)/lib/libibRHTTP.lib) \
+                               $(shell cygpath -w $(ROOTSYS)/lib/libROOTWebDisplay.lib) \
+                               $(shell cygpath -w $(ROOTSYS)/lib/libROOTQt5WebDisplaylib) \
                                $(shell cygpath -w $(ROOTSYS)/lib/libWebGui6.lib) \
                                 ../../lib/libWebGui6Full.lib"
 else
