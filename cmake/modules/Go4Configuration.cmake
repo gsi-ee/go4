14f2f7c0c7506677a4afd8f5745dbfc6189d4c53
Sergey Linev <S.Linev@gsi.de>
2020-05-13 12:21:54 +0000

    Generate files required for standalone analysis builds
    git-svn-id: https://subversion.gsi.de/go4/trunk@2804 302e7798-3865-0410-85c9-d38765bbeff6

--- cmake/modules/Go4Configuration.cmake
+++ cmake/modules/Go4Configuration.cmake
@@ -1,1 +1,17 @@
 set(LIBS_BASESET ${ROOT_LIBRARIES} XMLIO)
+
+set(GO4_DEFINITIONS "Linux" CACHE STRING "Go4 definitions")
+set(GO4_CXX_FLAGS "" CACHE STRING "Go4 CXX flags")
+set(GO4_C_FLAGS "" CACHE STRING "Go4 C flags")
+
+configure_file(${CMAKE_SOURCE_DIR}/cmake/scripts/Go4Config.cmake.in
+               ${CMAKE_BINARY_DIR}/Go4Config.cmake @ONLY NEWLINE_STYLE UNIX)
+
+configure_file(${CMAKE_SOURCE_DIR}/cmake/modules/Go4Macros.cmake
+               ${CMAKE_BINARY_DIR}/Go4Macros.cmake COPYONLY)
+
+set(GO4_ROOTDIR $ENV{ROOTSYS})
+
+configure_file(${CMAKE_SOURCE_DIR}/cmake/scripts/Go4UseFile.cmake.in
+               ${CMAKE_BINARY_DIR}/Go4UseFile.cmake @ONLY NEWLINE_STYLE UNIX)
+
