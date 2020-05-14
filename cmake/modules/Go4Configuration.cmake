e0bc32700e04b3e14c0b84958eb1660d6ab721c7
Sergey Linev <S.Linev@gsi.de>
2020-05-14 16:15:25 +0000

    Compile user guis with cmake
    Contrary to default make files, necessary classes extracted to 
    shared libraries and used correctly in UserGui.
    
    git-svn-id: https://subversion.gsi.de/go4/trunk@2829 302e7798-3865-0410-85c9-d38765bbeff6

--- cmake/modules/Go4Configuration.cmake
+++ cmake/modules/Go4Configuration.cmake
@@ -1,16 +1,23 @@
 set(GO4_WITH_RFIO)
 
-set(_platfrom Linux)
+set(_platform Linux)
 if(APPLE)
    set(_platform Darwin)
 elseif(MSVC)
    set(_platform WIN32)
 endif()
 
+set(_qtvers Qt5)
+if(qt4)
+   set(_qtvers Qt4)
+endif()
+   
+
 set(GO4_PLATFORM "${_platform}" CACHE STRING "Go4 platform definitions")
 set(GO4_DEFINITIONS "" CACHE STRING "Go4 extra definitions")
 set(GO4_CXX_FLAGS "" CACHE STRING "Go4 CXX flags")
 set(GO4_C_FLAGS "" CACHE STRING "Go4 C flags")
+set(GO4_QTVERSION "${_qtvers}" CACHE STRING "Go4 qt version")
 
 configure_file(${CMAKE_SOURCE_DIR}/cmake/scripts/Go4Config.cmake.in
                ${CMAKE_BINARY_DIR}/Go4Config.cmake @ONLY NEWLINE_STYLE UNIX)
@@ -22,4 +29,5 @@ set(GO4_ROOTDIR $ENV{ROOTSYS})
 
 configure_file(${CMAKE_SOURCE_DIR}/cmake/scripts/Go4UseFile.cmake.in
                ${CMAKE_BINARY_DIR}/Go4UseFile.cmake @ONLY NEWLINE_STYLE UNIX)
+
 
