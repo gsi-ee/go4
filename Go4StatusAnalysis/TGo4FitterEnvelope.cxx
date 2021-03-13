fc42e8ed2ecf01ff517cb8c957e5d5f2b71ae776
Sergey Linev <S.Linev@gsi.de>
2021-03-13 13:32:14 +0000

    use snrpintf, simplify PrintParameter
    git-svn-id: https://subversion.gsi.de/go4/trunk@3078 302e7798-3865-0410-85c9-d38765bbeff6

--- Go4StatusAnalysis/TGo4FitterEnvelope.cxx
+++ Go4StatusAnalysis/TGo4FitterEnvelope.cxx
@@ -31,19 +31,14 @@ TGo4FitterEnvelope::~TGo4FitterEnvelope()
 
 Int_t TGo4FitterEnvelope::PrintParameter(Text_t * buffer, Int_t buflen)
 {
-   Int_t locallen=128000;
    Text_t localbuf[128000];
    if(buflen<0 && buffer!=0)
       return 0;
-   Int_t size=0;
-   Text_t* current=localbuf;
-   size+=TGo4Parameter::PrintParameter(current,locallen-size-1);
-   current= localbuf + size;
+   Int_t size = TGo4Parameter::PrintParameter(localbuf, sizeof(localbuf)-1);
    if(fxFitter)
-      size+=sprintf(current," \t This object contains the fitter: %s \n",fxFitter->GetName());
+      size += snprintf(localbuf+size, sizeof(localbuf) - size, " \t This object contains the fitter: %s \n",fxFitter->GetName());
    else
-      size+=sprintf(current," \t This object contains no fitter\n");
-   current=localbuf+size;
+      size += snprintf(localbuf + size, sizeof(localbuf) - size, " \t This object contains no fitter\n");
    // here we optionally might put printout of fitter if we like...
    if(buffer==0)
    {
@@ -51,8 +46,8 @@ Int_t TGo4FitterEnvelope::PrintParameter(Text_t * buffer, Int_t buflen)
    }
    else
    {
-      if(size>buflen-1)
-         size=buflen-1;
+      if(size > buflen-1)
+         size = buflen-1;
       strncpy(buffer,localbuf,size);
    }
    return size;
@@ -62,7 +57,7 @@ Bool_t TGo4FitterEnvelope::UpdateFrom(TGo4Parameter *pp)
 {
    TGo4FitterEnvelope * from = dynamic_cast<TGo4FitterEnvelope *> (pp);
 
-   if(from==0) return kFALSE;
+   if(!from) return kFALSE;
 
    if(fxFitter!=0) delete fxFitter;
    fxFitter = from->GetFitter(kTRUE);// change ownership to us
