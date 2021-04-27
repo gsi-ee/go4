4a6ab51e63f9438eddbe66a3e9042f0a5ee504ba
Sergey Linev <S.Linev@gsi.de>
2021-04-27 10:30:34 +0000

    Install main files
    git-svn-id: https://subversion.gsi.de/go4/trunk@3187 302e7798-3865-0410-85c9-d38765bbeff6

--- cmake/modules/Go4InstallDirs.cmake
+++ cmake/modules/Go4InstallDirs.cmake
@@ -5,7 +5,7 @@ endif()
 
 if(NOT DEFINED CMAKE_INSTALL_LIBDIR)
   if(gnuinstall)
-    set(CMAKE_INSTALL_LIBDIR "lib/root" CACHE PATH "object code libraries (lib/root)")
+    set(CMAKE_INSTALL_LIBDIR "lib/go4" CACHE PATH "object code libraries (lib/go4)")
   else()
     set(CMAKE_INSTALL_LIBDIR "lib" CACHE PATH "object code libraries (lib)")
   endif()
@@ -13,17 +13,9 @@ endif()
 
 if(NOT DEFINED CMAKE_INSTALL_INCLUDEDIR)
   if(gnuinstall)
-    set(CMAKE_INSTALL_INCLUDEDIR "include/root" CACHE PATH "C header files (include/root)")
+    set(CMAKE_INSTALL_INCLUDEDIR "include/go4" CACHE PATH "C header files (include/go4)")
   else()
     set(CMAKE_INSTALL_INCLUDEDIR "include" CACHE PATH "C header files (include)")
-  endif()
-endif()
-
-if(NOT DEFINED CMAKE_INSTALL_SYSCONFDIR)
-  if(gnuinstall)
-    set(CMAKE_INSTALL_SYSCONFDIR "etc/root" CACHE PATH "read-only single-machine data (etc/root)")
-  else()
-    set(CMAKE_INSTALL_SYSCONFDIR "etc" CACHE PATH "read-only single-machine data (etc)")
   endif()
 endif()
 
@@ -44,4 +36,63 @@ if(NOT CMAKE_INSTALL_MACROSDIR)
     set(CMAKE_INSTALL_MACROSDIR "macros")
   endif()
 endif()
+
+if(NOT CMAKE_INSTALL_ETCDIR)
+  set(CMAKE_INSTALL_ETCDIR "" CACHE PATH "etc (DATADIR/etc)")
+  if(gnuinstall)
+    set(CMAKE_INSTALL_ETCDIR "${CMAKE_INSTALL_DATADIR}/etc")
+  else()
+    set(CMAKE_INSTALL_ETCDIR "etc")
+  endif()
+endif()
+
+if(NOT CMAKE_INSTALL_DOCSDIR)
+  set(CMAKE_INSTALL_DOCSDIR "" CACHE PATH "docs (DATADIR/docs)")
+  if(gnuinstall)
+    set(CMAKE_INSTALL_DOCSDIR "${CMAKE_INSTALL_DATADIR}/docs")
+  else()
+    set(CMAKE_INSTALL_DOCSDIR "docs")
+  endif()
+endif()
+
+if(NOT CMAKE_INSTALL_BUILDDIR)
+  set(CMAKE_INSTALL_BUILDDIR "" CACHE PATH "build (DATADIR/build)")
+  if(gnuinstall)
+    set(CMAKE_INSTALL_BUILDDIR "${CMAKE_INSTALL_DATADIR}/build")
+  else()
+    set(CMAKE_INSTALL_BUILDDIR "build")
+  endif()
+endif()
+
+if(NOT CMAKE_INSTALL_HTMLDIR)
+  set(CMAKE_INSTALL_HTMLDIR "" CACHE PATH "html (DATADIR/html)")
+  if(gnuinstall)
+    set(CMAKE_INSTALL_HTMLDIR "${CMAKE_INSTALL_DATADIR}/html")
+  else()
+    set(CMAKE_INSTALL_HTMLDIR "html")
+  endif()
+endif()
+
+if(NOT CMAKE_INSTALL_MAINDIR)
+  set(CMAKE_INSTALL_MAINDIR "" CACHE PATH "main dir (DATADIR)")
+  if(gnuinstall)
+    set(CMAKE_INSTALL_MAINDIR "${CMAKE_INSTALL_DATADIR}")
+  else()
+    set(CMAKE_INSTALL_MAINDIR "${CMAKE_INSTALL_PREFIX}")
+  endif()
+endif()
+
+mark_as_advanced(
+  CMAKE_INSTALL_BINDIR
+  CMAKE_INSTALL_LIBDIR
+  CMAKE_INSTALL_INCLUDEDIR
+  CMAKE_INSTALL_ICONSDIR
+  CMAKE_INSTALL_MACROSDIR
+  CMAKE_INSTALL_ETCDIR
+  CMAKE_INSTALL_DOCSDIR
+  CMAKE_INSTALL_BUILDDIR
+  CMAKE_INSTALL_HTMLDIR
+  CMAKE_INSTALL_MAINDIR
+)
+
 
