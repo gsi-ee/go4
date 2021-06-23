0a6a6dc080454a3a0a6272dcc4cdde22e54d136a
Sergey Linev <S.Linev@gsi.de>
2021-06-23 09:11:04 +0000

    add ROOT installation to windows package
    git-svn-id: https://subversion.gsi.de/go4/trunk@3244 302e7798-3865-0410-85c9-d38765bbeff6

---
+++ cmake/scripts/go4login.bat
@@ -0,0 +1,35 @@
+:: detect path where script is located
+
+:: configure Qt5 location
+
+@set QTDIR=C:/Qt5/5.15.2/msvc2019
+@set LIB=%QTDIR%/lib;%LIB%
+@set PATH=%QTDIR%/bin;%PATH%
+
+:: detect current dir
+
+@set OLDPATH=%cd%
+@set THIS=%0
+@set THIS=%THIS:~0,-12%.
+@cd /D %THIS%
+@set GO4SYS=%cd%
+@cd /D %OLDPATH%
+
+:: configure ROOT distributed with go4
+
+call %GO4SYS%/root/bin/thisroot.bat
+
+@set PATH=%GO4SYS%\bin;%PATH%
+@set LIB=.;%GO4SYS%\lib;%LIB%
+
+:: show configuration
+
+@echo Set environment for root and go4
+@echo    GO4SYS = %GO4SYS%
+@echo    ROOTSYS = %ROOTSYS%
+@echo    QTDIR = %QTDIR%
+
+:: if one need to compile user analysis, following lines should be
+:: uncommented and adjusted to actual location of MS Visual
+
+:: call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat"
