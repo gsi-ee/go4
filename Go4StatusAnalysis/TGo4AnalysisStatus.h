083e0d58eb2c07d576a4896e746be7db53c06827
Sergey Linev <S.Linev@gsi.de>
2022-02-16 11:46:30 +0100

    Replace PrintStatus -> Print for TGo4AnalysisStatus

--- Go4StatusAnalysis/TGo4AnalysisStatus.h
+++ Go4StatusAnalysis/TGo4AnalysisStatus.h
@@ -79,63 +79,62 @@ class TGo4AnalysisStatus : public TGo4Status {
     void ResetStepIterator();
 
     /** Gives next analysis step status. */
-    TGo4AnalysisStepStatus * NextStepStatus();
+    TGo4AnalysisStepStatus *NextStepStatus();
 
     /** Gives number of steps. */
-    Int_t GetNumberOfSteps();
+    Int_t GetNumberOfSteps() const;
 
     /** Gives analysis step status with provided index. */
-    TGo4AnalysisStepStatus* GetStepStatus(Int_t indx);
+    TGo4AnalysisStepStatus* GetStepStatus(Int_t indx) const;
 
     /** basic method to printout status information
       * on stdout; to be overridden by specific subclass */
-    virtual Int_t PrintStatus(Text_t* buffer=0, Int_t buflen=0);
-
+    void Print(Option_t* = "") const override;
 
   protected:
 
     /** Array containing all analysis steps. The steplist is iterated by
       * the maincycle from start to end index to process the active steps. */
-    TObjArray* fxStepArray;
+    TObjArray* fxStepArray{nullptr};
 
     /** Mutex protecting the analysis step list. */
-    TMutex * fxStepMutex; //!
+    TMutex * fxStepMutex{nullptr}; //!
 
     /** Iterator used for analysis step list. */
-    TIterator* fxStepIterator; //!
+    TIterator* fxStepIterator{nullptr}; //!
 
     /** True if chain of steps is checked for consistency of input and output events.
       * Default is true. Can be disabled for steps working in non-subsequent manner.
       * i.e. intermediate steps may be disabled, event structures of subsequent steps
       * need not match.  */
-    Int_t fbStepCheckingMode;
+    Int_t fbStepCheckingMode{1};
 
     /** Index describing the first analysis step from the steplist
       * to be processed. The first analysis step will use its own event
       * source to get input events. */
-    Int_t fiFirstStepIndex;
+    Int_t fiFirstStepIndex{0};
 
     /** Index describing the last  analysis step from the steplist
       * to be processed. Output event of the last analysis step is
       * accessible by GetEvent method and is used
       * for user event function and dynamic list. */
-    Int_t fiLastStepIndex;
+    Int_t fiLastStepIndex{0};
 
     /** Implicit Loop cycles in between two autosave actions. Autosave will write
       * calibration objects of steps into event file and will save all histograms into an default
       * root file. */
-    Int_t fiAutoSaveInterval;
+    Int_t fiAutoSaveInterval{0};
 
     /** compression level for autosave file */
-    Int_t fiAutoSaveCompression;
+    Int_t fiAutoSaveCompression{5};
 
     /** overwrite mode for autosave yes or no.
       * Internally, we use Int_t for correct streamer! */
-    Int_t fbAutoSaveOverwrite;
+    Int_t fbAutoSaveOverwrite{0};
 
     /** True if Autosaving is enabled (default).
       * If false, calling AutoSave() will have no effect. */
-    Int_t fbAutoSaveOn;
+    Int_t fbAutoSaveOn{1};
 
     /** Name of the autosave file. */
     TString fxAutoFileName;
@@ -143,7 +142,7 @@ class TGo4AnalysisStatus : public TGo4Status {
     /** Name of the last configuration file. */
     TString fxConfigFileName;
 
-  ClassDef(TGo4AnalysisStatus,4)
+  ClassDefOverride(TGo4AnalysisStatus,4)
 
 };
 
