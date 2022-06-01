f5fbc912b01c149073ec2260bd81862d26417e50
Sergey Linev <S.Linev@gsi.de>
2022-06-01 13:03:22 +0000

    Better formating of TGo4EventInfo
    git-svn-id: https://subversion.gsi.de/go4/trunk@3698 302e7798-3865-0410-85c9-d38765bbeff6

--- qt4/Go4GUI/TGo4EventInfo.cpp
+++ qt4/Go4GUI/TGo4EventInfo.cpp
@@ -17,7 +17,7 @@
 #include "TGo4Slot.h"
 #include "TGo4BrowserProxy.h"
 
-const char* defMbsEventName = "MbsEvent101";
+const char *defMbsEventName = "MbsEvent101";
 
 TGo4EventInfo::TGo4EventInfo(QWidget *parent, const char* name)
    : QGo4Widget(parent, name)
@@ -32,12 +32,12 @@ TGo4EventInfo::TGo4EventInfo(QWidget *parent, const char* name)
 
 bool TGo4EventInfo::IsAcceptDrag(const char* itemname, TClass* cl, int kind)
 {
-   return (kind==TGo4Access::kndEventElement);
+   return kind == TGo4Access::kndEventElement;
 }
 
 void TGo4EventInfo::DropItem(const char* itemname, TClass* cl, int kind)
 {
-   if (kind==TGo4Access::kndEventElement)
+   if (kind == TGo4Access::kndEventElement)
      WorkWithEvent(itemname);
 }
 
@@ -49,7 +49,7 @@ void TGo4EventInfo::WorkWithEvent(const char* itemname)
 
    MbsPrintBox->setEnabled(ismbs);
    TString mbsitem = Browser()->FindItemInAnalysis(defMbsEventName);
-   MbsButton->setEnabled(!ismbs && (mbsitem.Length()>0));
+   MbsButton->setEnabled(!ismbs && (mbsitem.Length() > 0));
    setFocus();
 }
 
@@ -58,7 +58,7 @@ void TGo4EventInfo::ResetWidget()
    QGo4Widget::ResetWidget();
    EventLbl->setText("");
    MbsButton->setEnabled(true);
-//   if (Browser()!=0) {
+//   if (Browser()) {
 //     TString mbsitem = Browser()->FindItemInAnalysis(defMbsEventName);
 //     MbsButton->setEnabled(mbsitem.Length()>0);
 //   } else
@@ -69,18 +69,18 @@ void TGo4EventInfo::ResetWidget()
 void TGo4EventInfo::linkedObjectUpdated(const char* linkname, TObject* obj)
 {
    TTree* tr = dynamic_cast<TTree*>(obj);
-   if (tr!=0) {
+   if (tr) {
       tr->Show(0);
       return;
    }
 
-   if (obj!=0) {
+   if (obj) {
       obj->Print();
       return;
    }
 }
 
-void TGo4EventInfo::linkedObjectRemoved(const char* linkname)
+void TGo4EventInfo::linkedObjectRemoved(const char * /*linkname*/)
 {
 }
 
@@ -89,21 +89,19 @@ void TGo4EventInfo::RefreshClicked()
 {
    RemoveAllLinks();
 
-   if (EventLbl->text().length()==0) return;
+   if (EventLbl->text().length() == 0) return;
    QString evname = EventLbl->text();
    bool isremote = RemoteButton->isChecked();
    bool istree = TreeButton->isChecked();
 
-   TGo4Slot* tgtslot = 0;
-   if (!isremote)
-     tgtslot = AddSlot("Event");
+   TGo4Slot* tgtslot = isremote ? nullptr : AddSlot("Event");
 
    TGo4BrowserProxy* br = Browser();
-   if (br==0) return;
+   if (!br) return;
 
    TString objname;
    TGo4ServerProxy* an = br->DefineAnalysisObject(evname.toLatin1().constData(), objname);
-   if (an!=0) an->RequestEventStatus(objname.Data(), istree, tgtslot);
+   if (an) an->RequestEventStatus(objname.Data(), istree, tgtslot);
 }
 
 void TGo4EventInfo::PrintEventClicked()
@@ -115,7 +113,7 @@ void TGo4EventInfo::PrintEventClicked()
 
    TGo4ServerProxy* anal = GetAnalysis(EventLbl->text().toLatin1().constData());
 
-   if (anal!=0)
+   if (anal)
      anal->RemotePrintEvent(name.Data(),
                             PrintEventNum->value(),
                             PrintEventSid->value(),
@@ -126,6 +124,9 @@ void TGo4EventInfo::PrintEventClicked()
 void TGo4EventInfo::MbsButton_clicked()
 {
    TString itemname = Browser()->FindItemInAnalysis(defMbsEventName);
-   if (itemname.Length()>0)
+
+   printf("Search item %s  found %s\n", defMbsEventName, itemname.Data());
+
+   if (itemname.Length() > 0)
      WorkWithEvent(itemname.Data());
 }
