d39a375724b172e54649c752b143317f4d639ac3
Jörn Adamczewski-Musch <j.adamczewski@gsi.de>
2025-03-17 10:22:02 +0100

    Updated version number of master

--- Go4Analysis/TGo4Version.h
+++ Go4Analysis/TGo4Version.h
@@ -21,8 +21,8 @@
 // to check the user analysis against framework version on runtime
 // 10205 means Version 1.02.05 or release number  1.2-5,
 
-#define __GO4BUILDVERSION__ 60399
-#define __GO4RELEASE__ "v6.3.99"
+#define __GO4BUILDVERSION__ 60499
+#define __GO4RELEASE__ "v6.4.99"
 
 class TGo4Version {
    protected:
