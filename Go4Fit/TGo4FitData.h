9e550d9f5c355005f53fc79417d60146ae703090
Sergey Linev <S.Linev@gsi.de>
2026-09-22 14:59:25 +0200

    Do not use RVersion.h to support unique_ptr
    c++14 is minimal standard for supported ROOT and therefore no need for workarounds

--- Go4Fit/TGo4FitData.h
+++ Go4Fit/TGo4FitData.h
@@ -19,11 +19,6 @@
 #include "TArrayI.h"
 #include "TArrayD.h"
 #include <memory>
-#include "RVersion.h"
-
-#if ROOT_VERSION_CODE < ROOT_VERSION(6,26,0)
-#include "ROOT/RMakeUnique.hxx"
-#endif
 
 class TH1;
 class TGraph;
